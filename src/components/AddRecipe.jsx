7fb7d6ced378efbb1442c13daca655ceb8779114
Safna-123 <ssafna327@gmail.com>
2024-05-06 12:51:19 +0530

    recipe app

---
+++ src/components/AddRecipe.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const AddRecipe = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Recipe Title</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">The Creator</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Description</label>
+                        <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Image</label>
+                        <input type="file" name="" id="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Recipes</label>
+                        <select name="" id="" className="form-select">
+                            <option value="Select Recipe">Select Recipe</option>
+                            <option value="">Chicken Biriyani</option>
+                            <option value="">Paneer Bhurji</option>
+                            <option value="">Alfam</option>
+                            <option value="">Masala Dosa</option>
+                            <option value="">Friedrice</option>
+                            <option value="">Veg Noodles</option>
+                            <option value="">Macroni</option>
+                        </select>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Ingredients</label>
+                        <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddRecipe
